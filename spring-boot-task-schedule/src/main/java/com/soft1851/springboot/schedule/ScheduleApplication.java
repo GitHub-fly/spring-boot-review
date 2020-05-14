3659850c8296755a703c961f2ff4d3a8a75959cd
flygit <1224178565@qq.com>
2020-05-14 21:39:36 +0800

    任务调度模块

---
+++ spring-boot-task-schedule/src/main/java/com/soft1851/springboot/schedule/ScheduleApplication.java
@@ -0,0 +1,19 @@
+package com.soft1851.springboot.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author xunmi
+ * @description: 通过@EnableScheduling开启定时
+ */
+@SpringBootApplication
+@EnableScheduling
+public class ScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleApplication.class, args);
+    }
+
+}
---
+++ spring-boot-task-schedule/src/main/java/com/soft1851/springboot/schedule/config/ScheduledConfig.java
@@ -0,0 +1,31 @@
+package com.soft1851.springboot.schedule.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * @author xunmi
+ * @ClassName ScheduledConfig
+ * @Description TODO
+ * @Date 2020/5/14
+ * @Version 1.0
+ **/
+@Configuration
+public class ScheduledConfig implements SchedulingConfigurer {
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(setTaskExecutors());
+    }
+
+    @Bean
+    public Executor setTaskExecutors() {
+        // 创建一个基本大小为 3 的线程池
+        return Executors.newScheduledThreadPool(3);
+    }
+}
