90ee12aa69813681d47070b16011b9b53b0307ea
flygit <1224178565@qq.com>
2020-05-18 23:06:53 +0800

    创建新模块quartz并完成简单的任务调度接口

---
+++ spring-boot-quartz/src/main/java/com/soft1851/springboot/quartz/SpringBootQuartzApplication.java
@@ -0,0 +1,16 @@
+package com.soft1851.springboot.quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xunmi
+ */
+@SpringBootApplication
+public class SpringBootQuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootQuartzApplication.class, args);
+    }
+
+}
